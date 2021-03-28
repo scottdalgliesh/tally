afd23b9e34745d920e7509afb091773216a98379
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2021-03-28 19:50:02 -0400

    add user model test

---
+++ tests/auth/test_models.py
@@ -0,0 +1,14 @@
+# pylint: disable=unused-argument
+from tally import bcrypt
+from tally.auth.models import User
+
+
+def test_user_model(session):
+    user = User.query.filter_by(username="scott").first()
+    assert user.username == "scott"
+    assert bcrypt.check_password_hash(user.password, "123")
+
+    categories = [categ.name for categ in user.categories]
+    bills = [bill.descr for bill in user.bills]
+    assert sorted(categories) == sorted(["groceries", "gas", "misc"])
+    assert sorted(bills) == sorted(["zehrs", "walmart", "ren's", "sobeys"])
